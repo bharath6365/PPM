35fd93817c7b58fc977ea44708ff8ac760830a50
bharathkumarc <bharathc@freshdesk.com>
2020-04-19 14:24:42 +0530

    Fix bugs with repository

--- backend/src/main/java/com/bharath/projectmanagement/repositories/BacklogRepository.java
+++ backend/src/main/java/com/bharath/projectmanagement/repositories/BacklogRepository.java
@@ -3,10 +3,11 @@ package com.bharath.projectmanagement.repositories;
 import javax.persistence.Entity;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bharath.projectmanagement.domain.Backlog;
 
-@Entity
+@Repository
 public interface BacklogRepository extends CrudRepository<Backlog, Long> {
 
 }
--- backend/src/main/java/com/bharath/projectmanagement/repositories/ProjectTaskRepository.java
+++ backend/src/main/java/com/bharath/projectmanagement/repositories/ProjectTaskRepository.java
@@ -1,10 +1,12 @@
 package com.bharath.projectmanagement.repositories;
 
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.bharath.projectmanagement.domain.ProjectTask;
+
 @Repository
-// This tells Spring boot that the base object is that of a project task and the id is of type long.
-public interface ProjectTask extends CrudRepository<ProjectTask, Long> {
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
 
 }
