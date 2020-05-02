29f3c53a78ba09792edc090ae39261da6a8a7792
bharathkumarc <bharathc@freshdesk.com>
2020-05-02 16:35:42 +0530

    Add new component folder structure

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -5,11 +5,13 @@ import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
 
 import formErrorReducer from './formErrorReducer';
 import projectReducer from './projectReducer';
+import backlogReducer from './backlogReducer';
 
 export default combineReducers({
   // Form errors.
   formErrors: formErrorReducer,
   // Project Reducer.
   project: projectReducer,
+  backlog: backlogReducer,
   toastr: toastrReducer
 })
